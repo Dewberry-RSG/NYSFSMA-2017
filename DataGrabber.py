24fd5c5b9c5dd21d311706d684acbaf701cd03a6
Seth Lawler <slawler@dewberry.com>
2017-03-30 17:44:51 -0400

    plotter improved

--- DataGrabber.py
+++ DataGrabber.py
@@ -223,7 +223,14 @@ def GetURL(gage):
     url = 'https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no={}&referred_module=sw&period=&begin_date=1900-03-23&end_date=2020-03-23'.format(gage)
     return url
 
-def plot1(df1, df4, df5, Tides_Only):
+def plotsingle(df1, df4, df5, Tides_Only, order):
+    df = df1[['datetime', 'Stage']].copy()
+    df.set_index('datetime', inplace=True)
+    df = df.ix[start_plot:stop_plot]
+    x1 = df.index
+    y1 = df['Stage'].resample('6T').mean()
+    interpolated = y1.interpolate(method='cubic', order=order)
+
     fig, ax1 = plt.subplots()
     ax1.set_ylabel('Stage: Tidal', color='black')    
     ax1.plot(df4.index, df4[Tides_Only], 'r')
@@ -234,42 +241,64 @@ def plot1(df1, df4, df5, Tides_Only):
     axes = plt.gca()
     axes.grid()
 
-
     ax2 = ax1.twinx()
-    ax2.plot(df1['datetime'],df1['Stage'], 'black')
+    ax2.plot(interpolated, 'black')
     ax2.set_ylabel('Stage: Fluvial', color='black')  
     axes.set_xlim([df4.index[0],df4.index[-1]])
 
-def plot2(df2, df4, df5, Tides_Only):
-    fig, ax1 = plt.subplots()
-    ax1.set_ylabel('Stage: Tidal', color='black')    
-    ax1.plot(df4.index, df4[Tides_Only], 'r')
-    ax1.plot(df5.index, df5[Tides_Only], 'b')
-
-    ax1.set_xlabel('Time')
-    fig.autofmt_xdate()
-    axes = plt.gca()
-    axes.grid()
+def plotmultiple(df1, df2, df3, df4, df5, df1_name, df2_name, df3_name, Tides_Only,order, start_plot, stop_plot):
+    f, (ax_1, ax_2, ax_3) = plt.subplots(3, sharex=True, sharey=True)
+    df = df1[['datetime', 'Stage']].copy()
+    df.set_index('datetime', inplace=True)
+    df = df.ix[start_plot:stop_plot]
+    x1 = df.index
+    y1 = df['Stage'].resample('6T').mean()
+    interpolated = y1.interpolate(method='cubic', order=2)
 
 
-    ax2 = ax1.twinx()
-    ax2.plot(df2['datetime'],df2['Stage'], 'black')
-    ax2.set_ylabel('Stage: Fluvial', color='black')  
-    axes.set_xlim([df4.index[0],df4.index[-1]])
-
-def plot3(df3, df4, df5, Tides_Only):
-    fig, ax1 = plt.subplots()
-    ax1.set_ylabel('Stage: Tidal', color='black')    
-    ax1.plot(df4.index, df4[Tides_Only], 'r')
-    ax1.plot(df5.index, df5[Tides_Only], 'b')
-
-    ax1.set_xlabel('Time')
-    fig.autofmt_xdate()
-    axes = plt.gca()
-    axes.grid()
+    ax_1.set_ylabel('{}'.format(Tides_Only), color='black')     
+    ax_1.plot(df4.index, df4[Tides_Only], 'r')
+    ax_1.plot(df5.index, df5[Tides_Only], 'b')
+    ax_1a = ax_1.twinx()
+    ax_1a.plot(interpolated, 'black')
+    ax_1a.set_ylabel('{}'.format(df1_name), color='black') 
 
 
-    ax2 = ax1.twinx()
-    ax2.plot(df3['datetime'],df3['Stage'], 'black')
-    ax2.set_ylabel('Stage: Fluvial', color='black')  
-    axes.set_xlim([df4.index[0],df4.index[-1]])
+    df = df2[['datetime', 'Stage']].copy()
+    df.set_index('datetime', inplace=True)
+    df = df.ix[start_plot:stop_plot]
+    x1 = df.index
+    y1 = df['Stage'].resample('6T').mean()
+    interpolated = y1.interpolate(method='cubic', order=2)
+
+
+    ax_2.set_ylabel('{}'.format(Tides_Only), color='black')     
+    ax_2.plot(df4.index, df4[Tides_Only], 'r')
+    ax_2.plot(df5.index, df5[Tides_Only], 'b')
+    ax_2a = ax_2.twinx()
+    ax_2a.plot(interpolated, 'black')
+    ax_2a.set_ylabel('{}'.format(df2_name), color='black')  
+
+
+    #--df3
+    df = df2[['datetime', 'Stage']].copy()
+    df.set_index('datetime', inplace=True)
+    df = df.ix[start_plot:stop_plot]
+    x1 = df.index
+    y1 = df['Stage'].resample('6T').mean()
+    interpolated = y1.interpolate(method='cubic', order=2)
+
+
+    ax_3.set_ylabel('{}'.format(Tides_Only), color='black')    
+    ax_3.plot(df4.index, df4[Tides_Only], 'r')
+    ax_3.plot(df5.index, df5[Tides_Only], 'b')
+    ax_3a = ax_3.twinx()
+    ax_3a.plot(interpolated, 'black')
+    ax_3a.set_ylabel('{}'.format(df3_name), color='black')  
+    
+    
+    f.autofmt_xdate()
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+    #axes = f.gca()
+    #axes.set_xlim([df4.index[0],df4.index[-1]])
+    #axes.grid()
